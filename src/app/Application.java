a2830811573ec9f4a2e9e0e430ee281fa7d22999
Nicolas Morand <nicolas.morand@studentfr.ch>
2025-02-02 15:23:09 +0100

    Exercice terminé

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,17 +1,29 @@
 package app;
 
 public class Application {
-    
-        //
-        //              )        (                 ) (           (         (
-        //           ( /(   *   ))\ )        (  ( /( )\ )        )\ )  (   )\ )
-        //     (   ( )\())` )  /(()/((       )\ )\()|()/(  (    (()/(  )\ (()/(
-        //     )\  )((_)\  ( )(_))(_))\    (((_|(_)\ /(_)) )\    /(_)|((_) /(_))
-        //    ((_)((_)((_)(_(_()|_))((_)   )\___ ((_|_))_ ((_)  (_)) )\___(_))
-        //    __   _____ _____ ___ ___    ___ ___  ___  ___   ___ ___ ___   _
-        //    \ \ / / _ \_   _| _ \ __|  / __/ _ \|   \| __| |_ _/ __|_ _| | |
-        //     \ V / (_) || | |   / _|  | (_| (_) | |) | _|   | | (__ | |  |_|
-        //      \_/ \___/ |_| |_|_\___|  \___\___/|___/|___| |___\___|___| (_)
-        //
 
+    public static void main(String[] args) {
+        Ami ami1;
+        ami1 = new Ami("Bernard", 8);
+        System.out.println(ami1.nom + " peut encore boire " + ami1.nbreBiereBuvable + " bières.");
+
+        Ami ami2;
+        ami2 = new Ami("Jean", 4);
+        System.out.println(ami2.nom + " peut encore boire " + ami2.nbreBiereBuvable + " bières.");
+
+
+        Ami ami3;
+        ami3 = new Ami("Steve", 6);
+        System.out.println(ami3.nom + " peut encore boire " + ami3.nbreBiereBuvable + " bières.");
+
+        ami1.boitUneBiere();
+        ami2.boitUneBiere();
+        ami3.boitUneBiere();
+
+        System.out.println(ami1.nom + " peut encore boire " + ami1.nbreBiereBuvable + " bières.");
+        System.out.println(ami2.nom + " peut encore boire " + ami2.nbreBiereBuvable + " bières.");
+        System.out.println(ami3.nom + " peut encore boire " + ami3.nbreBiereBuvable + " bières.");
+
+
+    }
 }
